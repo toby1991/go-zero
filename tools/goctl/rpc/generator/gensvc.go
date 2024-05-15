4f01fe0d56041e8e82fc42839eee10876b823424
Toby Yan <me@tobyan.com>
2024-05-15 18:14:01 +0800

    fix: intercept
    Signed-off-by: Toby Yan <me@tobyan.com>

--- tools/goctl/rpc/generator/gensvc.go
+++ tools/goctl/rpc/generator/gensvc.go
@@ -35,7 +35,7 @@ func (g *Generator) GenSvc(ctx DirContext, _ parser.Proto, cfg *conf.Config) err
 	configImport := fmt.Sprintf(`"%v"`, ctx.GetConfig().Package)
 	entImport := fmt.Sprintf(`"%v"`, ctx.GetEnt().Package)
 	entMigrateImport := fmt.Sprintf(`"%v"`, pathx.JoinPackages(ctx.GetEnt().Package, "migrate"))
-	entInterceptImport := fmt.Sprintf(`_ "%v"`, pathx.JoinPackages(ctx.GetEnt().Package, "intercept"))
+	entInterceptImport := fmt.Sprintf(`"%v"`, pathx.JoinPackages(ctx.GetEnt().Package, "intercept"))
 	entRuntimeImport := fmt.Sprintf(`_ "%v"`, pathx.JoinPackages(ctx.GetEnt().Package, "runtime"))
 
 	imports := collection.NewSet()
