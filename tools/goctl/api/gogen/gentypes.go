7454546913298b839130d5b295ccb54dd7b53599
Toby Yan <me@tobyan.com>
2025-04-09 17:19:54 +0800

    feat: change tag
    Signed-off-by: Toby Yan <me@tobyan.com>

--- tools/goctl/api/gogen/gentypes.go
+++ tools/goctl/api/gogen/gentypes.go
@@ -8,12 +8,12 @@ import (
 	"path"
 	"strings"
 
-	"github.com/zeromicro/go-zero/tools/goctl/api/spec"
-	apiutil "github.com/zeromicro/go-zero/tools/goctl/api/util"
-	"github.com/zeromicro/go-zero/tools/goctl/config"
-	"github.com/zeromicro/go-zero/tools/goctl/internal/version"
-	"github.com/zeromicro/go-zero/tools/goctl/util"
-	"github.com/zeromicro/go-zero/tools/goctl/util/format"
+	"github.com/toby1991/go-zero/tools/goctl/api/spec"
+	apiutil "github.com/toby1991/go-zero/tools/goctl/api/util"
+	"github.com/toby1991/go-zero/tools/goctl/config"
+	"github.com/toby1991/go-zero/tools/goctl/internal/version"
+	"github.com/toby1991/go-zero/tools/goctl/util"
+	"github.com/toby1991/go-zero/tools/goctl/util/format"
 )
 
 const typesFile = "types"
--- tools/goctl/go.mod
+++ tools/goctl/go.mod
@@ -1,4 +1,4 @@
-module github.com/zeromicro/go-zero/tools/goctl
+module github.com/toby1991/go-zero/tools/goctl
 
 go 1.21
 
@@ -15,7 +15,7 @@ require (
 	github.com/withfig/autocomplete-tools/integrations/cobra v1.2.1
 	github.com/zeromicro/antlr v0.0.1
 	github.com/zeromicro/ddl-parser v1.0.5
-	github.com/zeromicro/go-zero v1.8.1
+	github.com/toby1991/go-zero v1.8.1
 	golang.org/x/text v0.22.0
 	google.golang.org/grpc v1.65.0
 	google.golang.org/protobuf v1.36.5
